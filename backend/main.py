b2c6a53166ebe7660d6dfb9add33bd117fc9c624
raghavsatija7-png <raghavsatija7@gmail.com>
2026-09-06 22:29:08 +0530

    Ready to be deployed

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://gen-ai-platform-for-automated-content-transformation-8k0dzxe94.vercel.app"],
     allow_credentials=False,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
